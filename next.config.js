6732425e9ddca6f707dbca06f87201e613144c36
Ali Lopez <zero.exu@gmail.com>
2022-02-22 18:20:32 -0300

    feature: expose environment  vars

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    APP_NAME: process.env.APP_NAME,
+    APP_TITLE: process.env.APP_TITLE,
+    APP_META: process.env.APP_META,
+    POKEAPI: process.env.POKEAPI,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
