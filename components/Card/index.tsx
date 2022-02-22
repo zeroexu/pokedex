4990a4723c8f3fad32acc6a4666fabf14b7ff582
Ali Lopez <zero.exu@gmail.com>
2022-02-21 23:53:50 -0300

    refactor: component folder should be Uppercase and singular

---
+++ components/Card/index.tsx
@@ -0,0 +1,8 @@
+import { pokemon } from '@/types/index';
+import { FC } from 'react';
+
+const Card: FC<pokemon> = ({}) => {
+  return <div></div>;
+};
+
+export default Card;
--- components/cards/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const Card: FC<pokemon> = ({ }) => {
-    return <div></div>;
-}
-
-export default Card
