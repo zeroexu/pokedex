66551e58817b99b6835fb83e45ec6b4e384b164b
Ali Lopez <zero.exu@gmail.com>
2022-02-22 19:01:11 -0300

    refactor: fix all names types

--- libs/types/pokemon.ts
+++
@@ -1,8 +0,0 @@
-import { universalLink } from '@/types/universalLink';
-
-export interface pokemon {
-  id: number;
-  name: string;
-  image: string;
-  types: universalLink[];
-}
--- libs/types/universalLink.ts
+++
@@ -1,4 +0,0 @@
-export interface universalLink {
-  name: string;
-  url: string;
-}
