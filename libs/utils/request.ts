43e2c419e224606a0592b6910d57a066d05cb6e1
Ali Lopez <zero.exu@gmail.com>
2022-02-23 01:38:41 -0300

    refactor: implements parser on request library

--- libs/utils/request.ts
+++ libs/utils/request.ts
@@ -1,9 +1,17 @@
 import Axios from 'axios';
+const camelcaseKeysDeep = require('camelcase-keys-deep');
+
 const axiosInstance = Axios.create();
 const get = (url: string, data: any = {}) => {
-  return axiosInstance.get(url, data).catch((e) => {
-    return { data: null, status: e.response.status };
-  });
+  return axiosInstance
+    .get(url, data)
+    .then((response) => {
+      response.data = camelcaseKeysDeep(data);
+      return response;
+    })
+    .catch((e) => {
+      return { data: null, status: e.response.status };
+    });
 };
 export const request = {
   get,
