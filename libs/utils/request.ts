bdeba0821da0b64a11d73f24b377612edceb2e3c
Ali Lopez <zero.exu@gmail.com>
2022-02-23 00:49:04 -0300

    refactor: add a extra behaviour to request catch

--- libs/utils/request.ts
+++ libs/utils/request.ts
@@ -1,8 +1,10 @@
 import Axios from 'axios';
 const axiosInstance = Axios.create();
-
+const get = (url: string, data: any = {}) => {
+  return axiosInstance.get(url, data).catch((e) => {
+    return { data: null, status: e.response.status };
+  });
+};
 export const request = {
-  get: (url: string, data: any = {}) => {
-    return axiosInstance.get(url, data);
-  },
+  get,
 };
