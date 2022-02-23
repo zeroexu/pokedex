5661656a3e766fe233d2d44b458daeedc6d1a394
Ali Lopez <zero.exu@gmail.com>
2022-02-23 19:20:31 -0300

    refactor: updates on tag component

--- components/Tag/index.tsx
+++ components/Tag/index.tsx
@@ -1,8 +1,8 @@
 import { universalLink } from '@/types/index';
 import { FC } from 'react';
 
-const Tag: FC<universalLink> = ({ name }) => {
-  return <div>{name}</div>;
+const Tag: FC<universalLink> = ({ type: { name } }) => {
+  return <span>{name} </span>;
 };
 
 export default Tag;
