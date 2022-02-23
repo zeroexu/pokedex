a1523fa6e0faa228a4a5762706da778e6138dea3
Ali Lopez <zero.exu@gmail.com>
2022-02-23 19:19:46 -0300

    refactor: add poke link to card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react';
 import Tag from '@/components/Tag';
 import { IPokemon } from '@/types/IPokemon';
+import PokeLink from '@/components/PokeLink';
 
 const Card: FC<IPokemon> = ({ name, types, sprites, id }) => (
   <div>
-    {types.map((pokemonType, index) => (
-      <Tag key={`card-tag-${id}-${index}`} {...pokemonType} />
-    ))}
+    <div>
+      {types.map((pokemonType, index) => (
+        <Tag key={`card-tag-${id}-${index}`} {...pokemonType} />
+      ))}
+    </div>
     <img src={sprites.frontDefault} />
     <h2>
-      #{id} - {name}
+      <PokeLink name={`#${id} - ${name}`} url={`/pokemon/${id}`} />
     </h2>
   </div>
 );
