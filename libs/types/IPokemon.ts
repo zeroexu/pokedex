7cdaad9174104b28378d8eb43fac68ae77d71d44
Ali Lopez <zero.exu@gmail.com>
2022-02-23 01:39:05 -0300

    refactor: improve types with camelcase

--- libs/types/IPokemon.ts
+++ libs/types/IPokemon.ts
@@ -2,18 +2,18 @@ import { IUniversalLink } from '@/types/IUniversalLink';
 
 export interface IPokemon {
   abilities: IAbility[];
-  base_experience: number;
+  baseExperience: number;
   forms: IUniversalLink[];
-  game_indices: IGameIndex[];
+  gameIndices: IGameIndex[];
   height: number;
-  held_items: IHeldItems;
+  heldItems: IHeldItems;
   id: number;
-  is_default: boolean;
-  location_area_encounters: string;
+  isDefault: boolean;
+  locationAreaEncounters: string;
   moves: IMove[];
   name: string;
   order: number;
-  past_types: any[];
+  pastTypes: any[];
   species: IUniversalLink;
   sprites: ISprites;
   stats: IStats[];
@@ -31,18 +31,18 @@ export interface IPokemonList {
 
 export interface IAbility {
   ability: IUniversalLink;
-  is_hidden: boolean;
+  isHidden: boolean;
   slot: number;
 }
 
 export interface IGameIndex {
-  game_index: number;
+  gameIndex: number;
   version: IUniversalLink;
 }
 
 export interface IHeldItems {
   item: IUniversalLink;
-  version_details: IRarity[];
+  versionDetails: IRarity[];
 }
 
 export interface IRarity {
@@ -51,18 +51,18 @@ export interface IRarity {
 }
 
 export interface IVersionGroupDetails {
-  level_learned_at: number;
-  move_learn_method: IUniversalLink;
-  version_group: IUniversalLink;
+  levelLearned_at: number;
+  moveLearn_method: IUniversalLink;
+  versionGroup: IUniversalLink;
 }
 
 export interface IMove {
   move: IUniversalLink;
-  version_group_details: IVersionGroupDetails[];
+  versionGroupDetails: IVersionGroupDetails[];
 }
 
 export interface IStats {
-  base_stat: number;
+  baseStat: number;
   effort: number;
   stat: IUniversalLink;
 }
@@ -73,13 +73,13 @@ export interface IPokeType {
 }
 
 export interface ISprites {
-  back_default: string;
-  back_female: string;
-  back_shiny: string;
-  back_shiny_female: string;
-  front_default: string;
-  front_female: string;
-  front_shiny: string;
-  front_shiny_female: string;
+  backDefault: string;
+  backFemale: string;
+  backShiny: string;
+  backShinyFemale: string;
+  frontDefault: string;
+  frontFemale: string;
+  frontShiny: string;
+  frontShinyFemale: string;
   [key: string]: any;
 }
