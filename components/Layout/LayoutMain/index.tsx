2d3bb8b7b1da46a558c630d47198d6bcb624f9a1
Ali Lopez <zero.exu@gmail.com>
2022-02-24 10:34:33 -0300

    refactor: add headers and footer at layout

--- components/Layout/LayoutMain/index.tsx
+++ components/Layout/LayoutMain/index.tsx
@@ -1,12 +1,14 @@
 import { ILayoutCore } from '@/types/ILayoutCore';
 import { FC } from 'react';
-import LayoutCore from '../LayoutCore';
+import LayoutCore from '@/components/Layout/LayoutCore';
+import Header from '@/components/Layout/Header';
+import Footer from '../Footer';
 
 const LayoutMain: FC<ILayoutCore> = ({ children, titlePage, metaPage }) => (
   <LayoutCore titlePage={titlePage} metaPage={metaPage}>
-    <header>simple header</header>
+    <Header isBack={false} />
     <main className="grid">{children}</main>
-    <footer>simple footer</footer>
+    <Footer />
   </LayoutCore>
 );
 export default LayoutMain;
