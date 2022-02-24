402f89387361bcfd2f927a401ef0ce7613158786
Ali Lopez <zero.exu@gmail.com>
2022-02-24 10:33:48 -0300

    feature: add footer component

---
+++ components/Layout/Footer/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <footer>
+      <p>
+        ©2019 GAME FREAK inc.
+        <br />
+        ©2019 Pokémon. ©1995-2019 Nintendo/Creatures Inc. /GAME FREAK inc.
+        <br />
+        ©2019 GAME FREAK inc. Licensed by Rising Star Games Ltd. and published
+        by Rainy Frog LLC.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
