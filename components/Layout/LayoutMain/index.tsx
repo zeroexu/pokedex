a96106ee276fa393bf876a454e22d0cba96292e0
Ali Lopez <zero.exu@gmail.com>
2022-02-25 10:15:26 -0300

    refactor: use list navigation on pages and layout

--- components/Layout/LayoutMain/index.tsx
+++ components/Layout/LayoutMain/index.tsx
@@ -1,13 +1,20 @@
-import { ILayoutCore } from '@/types/ILayoutCore';
+import { ILayoutGrid } from '@/types/ILayoutCore';
 import { FC } from 'react';
 import LayoutCore from '@/components/Layout/LayoutCore';
 import Header from '@/components/Layout/Header';
 import Footer from '../Footer';
 
-const LayoutMain: FC<ILayoutCore> = ({ children, titlePage, metaPage }) => (
+const LayoutMain: FC<ILayoutGrid> = ({
+  children,
+  titlePage,
+  metaPage,
+  paginator,
+}) => (
   <LayoutCore titlePage={titlePage} metaPage={metaPage}>
     <Header isBack={false} />
+    {paginator}
     <main className="grid">{children}</main>
+    {paginator}
     <Footer />
   </LayoutCore>
 );
