7de690c8e166ac360079b1827deb803979f5e888
Ali Lopez <zero.exu@gmail.com>
2022-02-25 09:17:55 -0300

    refactor: use species, size, and change sprite instead image

--- components/Description/index.tsx
+++ components/Description/index.tsx
@@ -1,30 +1,41 @@
 import { FC } from 'react';
 import GroupTag from '@/components/Tag/GroupTag';
 import { IPokemon } from '@/types/IPokemon';
-import PokeSprite from '../PokeSprite';
+import Species from '../Species';
+import Image from 'next/image';
+import SizeInfo from '../SizeInfo';
 
-const Description: FC<IPokemon> = ({ name, types, sprites, id }) => (
+const Description: FC<IPokemon> = ({
+  name,
+  types,
+  sprites,
+  id,
+  species,
+  height,
+  weight,
+}) => (
   <div className="description">
     <div>
-      <PokeSprite
-        url={sprites.other.dreamWorld.frontDefault}
-        name={name}
-        width={'100%'}
-        height={'100vh'}
-      />
+      <div className="next-image-container">
+        <Image
+          src={sprites.other.dreamWorld.frontDefault}
+          width="100px"
+          height="100px"
+          alt={name}
+          placeholder="empty"
+          layout="responsive"
+          objectFit="contain"
+        />
+      </div>
     </div>
-    <div>
+    <div className="card">
       <h2>
         #{id} - {name}
       </h2>
+      <Species {...species} />
+      <SizeInfo height={height} weight={weight} />
+      <h3>Tipos:</h3>
       <GroupTag nameGroup={'card-pokemon-description-'} id={id} types={types} />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-        eleifend eu dui vitae commodo. Aliquam eu purus viverra, bibendum nibh
-        vitae, cursus nunc. Proin in urna consectetur, dictum quam nec, viverra
-        lectus. Morbi dapibus nisl ut purus pretium pellentesque. Etiam
-        facilisis pulvinar tristique. Pellentesque sodales rhoncus maximus.
-      </p>
     </div>
   </div>
 );
