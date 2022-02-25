9fb3814fb298488f91acf712a0c873739560071d
Ali Lopez <zero.exu@gmail.com>
2022-02-25 09:18:50 -0300

    refactor: tag add nametype to has color

--- components/Tag/index.tsx
+++ components/Tag/index.tsx
@@ -2,7 +2,7 @@ import { IUniversalLink } from '@/types/IUniversalLink';
 import { FC } from 'react';
 
 const Tag: FC<IUniversalLink> = ({ name }) => {
-  return <span>{name} </span>;
+  return <span className={`tag ${name}`}>{name} </span>;
 };
 
 export default Tag;
